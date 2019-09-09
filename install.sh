04508af4d17cf7e129bfd753627383f00700dac3
nukisuke <nukisuke.0214@gmail.com>
2019-09-09 22:49:59 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # dein install
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/dotfiles/dein/installer.sh
-sh ~/dotfiles/dein/installer.sh ~/dotfiles/.cache/dein
+sh ~/dein/installer.sh ~/.cache/dein
+mv ~/.cache ~/dotfiles/
+rm ~/installer.sh
 
 ln -fs ~/dotfiles/_vimrc ~/.vimrc
 ln -fs ~/dotfiles/_zshrc ~/.zshrc
