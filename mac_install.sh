e2681d836d4f234805990fdb51dbeaa5974bf585
nusuke <nusukep@gmail.com>
2023-06-18 17:37:01 +0900

    update

--- mac_install.sh
+++ mac_install.sh
@@ -1,9 +1,10 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install bat
 brew install fzf
 brew install ripgrep
+brew install fd
 brew install exa
 brew install ghq
-brew install hub
 
 git config --global ghq.root ~/src
