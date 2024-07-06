0fe6618f23830e7c6b764b17c05b69c874b7f748
nusuke <nusukep@gmail.com>
2024-07-06 16:03:15 +0900

    brew bundle

--- mac_install.sh
+++ mac_install.sh
@@ -1,14 +1,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew install bat
-brew install fzf
-brew install ripgrep
-brew install fd
-brew install exa
-brew install ghq
-brew install bottom
-brew install --cask raycast
-brew install --cask warp
-
+brew bundle
 
 git config --global ghq.root ~/src
